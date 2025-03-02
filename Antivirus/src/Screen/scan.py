ebe6b3fde13f7049b45348027772e13ecb60cef4
ParthGiriGoswami <143078824+ParthGiriGoswami@users.noreply.github.com>
2025-03-02 12:40:38 +0530

    Files modified and added

--- Antivirus/src/Screen/scan.py
+++ Antivirus/src/Screen/scan.py
@@ -2,7 +2,7 @@ import flet as ft
 import threading
 from Screen.Commonscan import scan_drives
 malware_count = set()
-def Scan(page: ft.Page, scanned, rule):
+def Scan(page: ft.Page, scanned, rule,flag):
     count=len(scanned)
     malware_count.clear()
     txt = ft.Text(value="",width=600,max_lines=1,overflow=ft.TextOverflow.ELLIPSIS,text_align=ft.TextAlign.CENTER)
@@ -31,6 +31,6 @@ def Scan(page: ft.Page, scanned, rule):
     page.update()
     threading.Thread(
         target=scan_drives, 
-        args=(page, txt, info,count, scanned, progress_ring, malware_count, rule,bs),
+        args=(page, txt, info,count, scanned, progress_ring, malware_count,rule,bs,flag),
         daemon=True
     ).start()
