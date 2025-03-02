1117b6d69ea42f5d5a09a1062299f2c9afa7b8db
ParthGiriGoswami <143078824+ParthGiriGoswami@users.noreply.github.com>
2025-03-02 12:39:50 +0530

    Heuristic scan

--- Antivirus/src/Screen/Homeview.py
+++ Antivirus/src/Screen/Homeview.py
@@ -1,41 +1,9 @@
 import flet as ft
 from Screen.scan import Scan
-import os
-scanned=set()
-def scan_directory(directory, file_set):
-    try:
-        with os.scandir(directory) as entries:
-            for entry in entries:
-                if entry.is_file():
-                    file_set.add(entry.path)
-                elif entry.is_dir(follow_symlinks=False):
-                    scan_directory(entry.path, file_set)
-    except (PermissionError, FileNotFoundError):
-        pass
-def on_folder_picked_for_quick_scan(e: ft.FilePickerResultEvent, page: ft.Page,rule,quickfiles):
-    global scanned
-    if e.path:
-        try:
-            quick_file_path = "src/Screen/quickpath.txt"
-            os.makedirs(os.path.dirname(quick_file_path), exist_ok=True)
-            try:
-                with open(quick_file_path, "a") as quick_file:
-                    quick_file.write(f"{e.path}\n")
-                    scanned.add(e.path)
-                for file in scanned:
-                    scan_directory(file,quickfiles)
-            except:
-                pass
-        except:
-            pass
-        if scanned:
-            Scan(page,quickfiles,rule)
-def HomeView(page: ft.Page,rule,quickfiles):
-    file_picker_for_quick_scan = ft.FilePicker(on_result=lambda e: on_folder_picked_for_quick_scan(e, page,rule,quickfiles))
-    page.overlay.append(file_picker_for_quick_scan)
+def HomeView(page: ft.Page,rule,files):
     btn1 = ft.ElevatedButton(
         "Quick scan",
-        on_click=lambda _:file_picker_for_quick_scan.get_directory_path() if not quickfiles else Scan(page,quickfiles,rule)
+        on_click=lambda _:Scan(page,files,rule,False)
     )
     return ft.Container(
         expand=True,
