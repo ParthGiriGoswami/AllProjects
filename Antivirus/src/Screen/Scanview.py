ca8a7f0411a7a3c4efcf9bb0f2e82c14c4b98b17
ParthGiriGoswami <143078824+ParthGiriGoswami@users.noreply.github.com>
2025-05-14 22:16:42 +0530

    Update Scanview.py

--- Antivirus/src/Screen/Scanview.py
+++ Antivirus/src/Screen/Scanview.py
@@ -13,8 +13,7 @@ def scan_directory(directory, file_set):
                     scan_directory(entry.path, file_set)
     except (PermissionError, FileNotFoundError):
         pass
-def on_folder_picked_for_quick_scan(e: ft.FilePickerResultEvent, page: ft.Page,rule,quickfiles):
-    global scanned
+def on_folder_picked_for_quick_scan(e: ft.FilePickerResultEvent, page: ft.Page,rule,quickfiles,quickpath):
     if e.path:
         try:
             quick_file_path = "storage/data/quickpath.txt"
@@ -22,6 +21,7 @@ def on_folder_picked_for_quick_scan(e: ft.FilePickerResultEvent, page: ft.Page,r
             try:
                 with open(quick_file_path, "a") as quick_file:
                     quick_file.write(f"{e.path}\n")
+                    quickpath.add(e.path)
                     scanned.add(e.path)
                 for file in scanned:
                     scan_directory(file,quickfiles)
@@ -43,9 +43,11 @@ def on_folder_picked_for_custom_scan(e: ft.FilePickerResultEvent, page: ft.Page,
             pass
         if scanned:
             Scan(page,scanned,rule,False)
-def ScanView(page: ft.Page,rule,quickfiles,deepfiles):
+def ScanView(page: ft.Page,rule,quickfiles,quickpath,deepfiles):
     file_picker_for_custom_scan = ft.FilePicker(on_result=lambda e: on_folder_picked_for_custom_scan(e, page,rule))
     page.overlay.append(file_picker_for_custom_scan)
+    file_picker_for_quick_scan = ft.FilePicker(on_result=lambda e: on_folder_picked_for_quick_scan(e, page,rule,quickfiles,quickpath))
+    page.overlay.append(file_picker_for_quick_scan)
     return ft.Container(
         expand=True,
         adaptive=True,
@@ -53,10 +55,10 @@ def ScanView(page: ft.Page,rule,quickfiles,deepfiles):
         content=ft.Column(
             [
                 ft.Text(value="Scans", size=20),
-                create_custom_button(page,"Quick Scan","Quickly scans high-risk areas for threats",100,icon=ft.Icons.SAVED_SEARCH,on_click=lambda _:Scan(page,quickfiles,rule,False)),
-                create_custom_button(page,"Deep Scan","A full threat inspection for your entire device",100,icon=ft.Icons.SCREEN_SEARCH_DESKTOP_SHARP,on_click=lambda _:Scan(page,deepfiles,rule,True)),
-                create_custom_button(page,"Custom Scan","Allows you to scan specific folders on your device",100,icon=ft.Icons.DASHBOARD_CUSTOMIZE_SHARP,on_click=lambda _: file_picker_for_custom_scan.get_directory_path()),
+                create_custom_button(page,"Quick Scan","Quickly scans high-risk areas for threats",icon=ft.Icons.SAVED_SEARCH,on_click=lambda _:file_picker_for_quick_scan.get_directory_path() if not quickfiles else Scan(page,quickfiles,rule,False)),
+                create_custom_button(page,"Deep Scan","A full threat inspection for your entire device",icon=ft.Icons.SCREEN_SEARCH_DESKTOP_SHARP,on_click=lambda _:Scan(page,deepfiles,rule,True)),
+                create_custom_button(page,"Custom Scan","Allows you to scan specific folders on your device",icon=ft.Icons.DASHBOARD_CUSTOMIZE_SHARP,on_click=lambda _: file_picker_for_custom_scan.get_directory_path()),
             ],
-            spacing=20,
+            spacing=21,
         ),
     )
