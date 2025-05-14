c7a42ac95fd8cb430dd99244ee6f120aa606d1b6
ParthGiriGoswami <143078824+ParthGiriGoswami@users.noreply.github.com>
2025-05-14 22:16:37 +0530

    Update Settingsview.py

--- Antivirus/src/Screen/Settingsview.py
+++ Antivirus/src/Screen/Settingsview.py
@@ -1,6 +1,7 @@
 import flet as ft
 from Screen.Createbutton import create_custom_button
 from Screen.FileDecryption import file_decryption
+from Screen.ListFiles import listfiles
 import os
 import subprocess
 def folder_unlocker(e: ft.FilePickerResultEvent, page: ft.Page):
@@ -43,57 +44,15 @@ def file_decryptor(e: ft.FilePickerResultEvent, page: ft.Page):
                 ),
             )
             page.open(dia)
-def on_folder_picked_for_quick_scan(e: ft.FilePickerResultEvent, page: ft.Page):
-    count=0
-    def handle_close(e):
-        page.close(dia)
-    if e.path:
-        try:
-            quick_file_path = "storage/data/quickpath.txt"
-            os.makedirs(os.path.dirname(quick_file_path), exist_ok=True)
-            try:
-                with open(quick_file_path, "a") as quick_file:
-                    quick_file.write(f"{e.path}\n")
-                    count=count+1
-            except:
-                pass
-        except:
-            pass
-        if (count==0):
-            dia=ft.AlertDialog(
-                modal=True,
-                title=ft.Text("Info"),
-                content=ft.Text("No files selected"),
-                actions=[
-                    ft.TextButton("Ok", on_click=handle_close),
-                ],
-                actions_alignment=ft.MainAxisAlignment.END,
-                on_dismiss=lambda e: page.add(
-                    ft.Text("Modal dialog dismissed"),
-                ),
-            )
-            page.open(dia)
-        else:
-            dia=ft.AlertDialog(
-                modal=True,
-                title=ft.Text("Info"),
-                content=ft.Text("A  path is added to quick scan"),
-                actions=[
-                    ft.TextButton("Ok", on_click=handle_close),
-                ],
-                actions_alignment=ft.MainAxisAlignment.END,
-                on_dismiss=lambda e: page.add(
-                    ft.Text("Modal dialog dismissed"),
-                ),
-            )
-            page.open(dia)
-def SettingsView(page: ft.Page):
-    file_picker_for_quick_scan = ft.FilePicker(on_result=lambda e: on_folder_picked_for_quick_scan(e, page))
-    page.overlay.append(file_picker_for_quick_scan)
+def SettingsView(page: ft.Page,quickpath,quickfile):
     file_decrypt = ft.FilePicker(on_result=lambda e: file_decryptor(e, page))
     page.overlay.append(file_decrypt)
     unlock_folder = ft.FilePicker(on_result=lambda e: folder_unlocker(e, page))
     page.overlay.append(unlock_folder)
+    exclusionpath=None
+    if os.path.exists("storage/data/exclusion.txt"):
+        with open("storage/data/exclusion.txt", "r") as file:
+            exclusionpath=set(line.strip() for line in file)
     return ft.Container(
         expand=True,
         padding=10,
@@ -101,10 +60,11 @@ def SettingsView(page: ft.Page):
         content=ft.Column(
             [
                 ft.Text(value="Settings", size=20),
-                create_custom_button(page,"Add Files","Adds files to quickscan",icon=ft.Icons.ADD_BOX,h=100,on_click=lambda _: file_picker_for_quick_scan.get_directory_path()),
-                create_custom_button(page,"File Decryption","Decrypt a file",icon=ft.Icons.LOCK_OPEN,h=100,on_click=lambda _:  file_decrypt.pick_files(allow_multiple=False)), 
-                create_custom_button(page,"Unlock Folder","Unlocks any locked folder in the device",icon=ft.Icons.MANAGE_ACCOUNTS_ROUNDED,h=100,on_click=lambda _:unlock_folder.get_directory_path())
+                create_custom_button(page,"Edit Quick files","Adds or remove files from quickscan list",icon=ft.Icons.ADD_BOX,on_click=lambda _: listfiles(page,idp="Quick List",path=quickpath,file=quickfile)),
+                create_custom_button(page,"Edit Exclusion files","Adds or remove files from exclusion list",icon=ft.Icons.ADD_BOX,on_click=lambda _: listfiles(page,idp="Exclusion List",path=exclusionpath)),
+                create_custom_button(page,"File Decryption","Decrypt a file",icon=ft.Icons.LOCK_OPEN,on_click=lambda _:  file_decrypt.pick_files(allow_multiple=False, allowed_extensions=["encrypted"])), 
+                create_custom_button(page,"Unlock Folder","Unlocks any locked folder in the device",icon=ft.Icons.MANAGE_ACCOUNTS_ROUNDED,on_click=lambda _:unlock_folder.get_directory_path()),
             ],
-            spacing=20,
+            spacing=21,
         ),
     )
